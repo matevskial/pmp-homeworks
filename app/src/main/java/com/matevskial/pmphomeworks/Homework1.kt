90a0254b37ae1834fd6b5c7629ba5afaa8fa7588
Aleksandar Matevski <matevskial@gmail.com>
2021-03-04 22:25:30 +0100

    [hm1] Set up UI

---
+++ app/src/main/java/com/matevskial/pmphomeworks/Homework1.kt
@@ -0,0 +1,12 @@
+package com.matevskial.pmphomeworks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Homework1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_homework1)
+        title = "Homework 1"
+    }
+}
